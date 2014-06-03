4cc136aaa4b21b070a56aed8656745981f10ba04
Guillaume Allegre <guillaume.allegre@silecs.info>
2014-06-03 13:43:56 +0200

    separate message strings

--- lang/en/local_resourcenotif.php
+++ lang/en/local_resourcenotif.php
@@ -12,15 +12,17 @@ $string['allstudentrecipient'] = 'This message will be sent to all users enrolle
 $string['body'] = 'Message body: ';
 $string['complement'] = 'Message complement: ';
 $string['grouprecipient'] = 'The following message will be sent to users '
-    . 'able to view <a href="{$a->linkactivity}">{$a->nameactivity}</a> ({$a->nbdest}).';
-$string['msgsender'] = '{$a->sender} wish to highlight the resource '
-    . '{$a->linkactivity} available into the course {$a->linkcourse}.';
+    . 'permitted to view <a href="{$a->linkactivity}">{$a->nameactivity}</a> ({$a->nbdest}).';
 $string['nomessagesend'] = 'No sent message.';
 $string['norecipient'] = 'No recipient.';
-$string['notification'] = 'Notification: ';
 $string['numbernotification'] = 'Number of notifications sent: {$a}';
 $string['returncourse'] = 'Return to course';
 $string['sender'] = 'Sender: ';
 $string['sendnotification'] = 'Send a notification';
 $string['subject'] = 'Object: ';
 $string['submit'] = 'Send';
+
+// The following strings are part of the sent message
+$string['notification'] = 'Notification: ';
+$string['msgsender'] = '{$a->sender} wish to highlight the resource '
+    . '{$a->linkactivity} available into the course {$a->linkcourse}.';
--- lang/fr/local_resourcenotif.php
+++ lang/fr/local_resourcenotif.php
@@ -11,16 +11,18 @@ $string['pluginname'] = 'Notification de ressource';
 $string['allstudentrecipient'] = 'Le message suivant sera transmis aux utilisateurs inscrits à ce cours ({$a}).';
 $string['body'] = 'Corps du message : ';
 $string['complement'] = 'Complément de message : ';
-$string['grouprecipient'] = 'Le message suivant sera transmis au utilisateurs '
+$string['grouprecipient'] = 'Le message suivant sera transmis aux utilisateurs '
     . 'concernés par <a href="{$a->linkactivity}">{$a->nameactivity}</a> ({$a->nbdest}).';
-$string['msgsender'] = '{$a->sender} souhaite attirer votre attention sur l\'élément '
-    . '{$a->linkactivity} proposé au sein du cours {$a->linkcourse}.';
 $string['nomessagesend'] = 'Aucun message envoyé.';
 $string['norecipient'] = 'Aucun destinataire';
-$string['notification'] = 'Notification : ';
 $string['numbernotification'] = 'Nombre de notifications envoyées : {$a}';
 $string['returncourse'] = 'Retour au cours';
 $string['sender'] = 'Expéditeur : ';
 $string['sendnotification'] = 'Envoyer une notification';
 $string['subject'] = 'Sujet : ';
 $string['submit'] = 'Envoyer';
+
+// The following strings are part of the sent message
+$string['notification'] = 'Notification : ';
+$string['msgsender'] = '{$a->sender} souhaite attirer votre attention sur l\'élément '
+    . '{$a->linkactivity} proposé au sein du cours {$a->linkcourse}.';
