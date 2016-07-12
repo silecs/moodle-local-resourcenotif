78c5379e97235119bdc32650cad0866249c699c0
François Gannaz <francois.gannaz@silecs.info>
2016-07-12 23:35:02 +0200

    Moodle 3.1 changed the way plugins are loaded => v3.0 required

--- lib.php
+++ lib.php
@@ -1,28 +1,31 @@
 <?php
 
-global $OUTPUT, $PAGE;
+function local_resourcenotif_extend_navigation_course() {
+    global $OUTPUT, $PAGE;
 
-$html = '<li role="presentation">'
-	. '<a class="editing_notifications menu-action cm-edit-action" data-action="notifications" role="menuitem" href="'
-	. htmlspecialchars(new moodle_url('/local/resourcenotif/resourcenotif.php', array('id' => '123XYZ321')))
-	. '" title="' . htmlspecialchars(get_string("notifications")) . '">'
-	. $OUTPUT->pix_icon('t/email', get_string("notifications"), 'moodle', array('class' => 'iconsmall', 'title' => ''))
-	. '<span class="menu-action-text">' . htmlspecialchars(get_string("notifications")) . '</span>'
-	. '</a>'
-	. '</li>';
-$enc = json_encode($html);
+    $html = '<li role="presentation">'
+        . '<a class="editing_notifications menu-action cm-edit-action" data-action="notifications" role="menuitem" href="'
+        . htmlspecialchars(new moodle_url('/local/resourcenotif/resourcenotif.php', array('id' => '123XYZ321')))
+        . '" title="' . htmlspecialchars(get_string("notifications")) . '">'
+        . $OUTPUT->pix_icon('t/email', get_string("notifications"), 'moodle', array('class' => 'iconsmall', 'title' => ''))
+        . '<span class="menu-action-text">' . htmlspecialchars(get_string("notifications")) . '</span>'
+        . '</a>'
+        . '</li>';
+    $enc = json_encode($html);
 
-$PAGE->requires->js_init_code(<<<EOJS
-var activities = document.querySelectorAll('.section-cm-edit-actions ul[role="menu"]');
-if (activities) {
-	for (var i = 0; i < activities.length; i++) {
-		var ul = activities[i];
-		var owner = ul.parentNode.getAttribute('data-owner');
-		if (owner) {
-			var id = owner.replace(/^#module-/, '');
-			ul.insertAdjacentHTML('beforeend', $enc.replace('123XYZ321', id));
-		}
-	}
-}
+    $PAGE->requires->js_init_code(<<<EOJS
+    var activities = document.querySelectorAll('.section-cm-edit-actions ul[role="menu"]');
+    if (activities) {
+        for (var i = 0; i < activities.length; i++) {
+            var ul = activities[i];
+            var owner = ul.parentNode.getAttribute('data-owner');
+            if (owner) {
+                var id = owner.replace(/^#module-/, '');
+                ul.insertAdjacentHTML('beforeend', $enc.replace('123XYZ321', id));
+            }
+        }
+    }
 EOJS
-, true);
+    , true);
+}
+
--- version.php
+++ version.php
@@ -2,14 +2,14 @@
 /**
  * @package    local
  * @subpackage resourcenotif
- * @copyright  2012-2014 Silecs {@link http://www.silecs.info/societe}
+ * @copyright  2012-2016 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016071203;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2013111802.09;        // Requires this Moodle version
-$plugin->component = 'local_resourcenotif';       // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2016071111;
+$plugin->requires  = 2015111600; // 3.0
+$plugin->component = 'local_resourcenotif';
 
-$maturity = MATURITY_STABLE;             // This version's maturity level.
+$maturity = MATURITY_STABLE;
