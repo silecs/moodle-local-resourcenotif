f15dc81e84b7de31acbfb6fc8276f67f90531d2e
François Gannaz <francois.gannaz@silecs.info>
2016-07-12 21:03:19 +0200

    JS hack: fixes the link

--- lib.php
+++ lib.php
@@ -2,12 +2,14 @@
 
 global $OUTPUT, $PAGE;
 
-$html = '<a class="editing_notifications menu-action cm-edit-action" data-action="notifications" role="menuitem" href="'
+$html = '<li role="presentation">'
+	. '<a class="editing_notifications menu-action cm-edit-action" data-action="notifications" role="menuitem" href="'
 	. htmlspecialchars(new moodle_url('/local/resourcenotif/resourcenotif.php', array('id' => '123XYZ321')))
 	. '" title="' . htmlspecialchars(get_string("notifications")) . '">'
 	. $OUTPUT->pix_icon('t/email', get_string("notifications"), 'moodle', array('class' => 'iconsmall', 'title' => ''))
 	. '<span class="menu-action-text">' . htmlspecialchars(get_string("notifications")) . '</span>'
-	. '</a>';
+	. '</a>'
+	. '</li>';
 $enc = json_encode($html);
 
 $PAGE->requires->js_init_code(<<<EOJS
@@ -17,7 +19,7 @@ if (activities) {
 		var ul = activities[i];
 		var owner = ul.parentNode.getAttribute('data-owner');
 		if (owner) {
-			var id = owner.replace(/^#module-/);
+			var id = owner.replace(/^#module-/, '');
 			ul.insertAdjacentHTML('beforeend', $enc.replace('123XYZ321', id));
 		}
 	}
