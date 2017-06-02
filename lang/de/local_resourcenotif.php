d75892b4bb18f59e27b40cd67f305e36042a2fbf
wehr <mariowehr@gmx.at>
2017-06-02 11:01:41 +0200

    Added german localization.

---
+++ lang/de/local_resourcenotif.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package    local
+ * @subpackage up1_notificationcourse
+ * @copyright  2012-2014 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Ressourcen Benachrichtigung';
+
+$string['allstudentrecipient'] = 'Diese Nachricht wird an ({$a}) User (Studenten), die in den Kurs eingeschrieben sind, versendet.';
+$string['body'] = 'Benachrichtigungsinhalt: ';
+$string['complement'] = 'Zusatz: ';
+$string['event'] = 'Benachrichtigung gesendet';
+$string['grouprecipient'] = 'Diese Benachrichtigung wird nur an User mit der Berechtigung zur Ansicht von <a href="{$a->linkactivity}">{$a->nameactivity}</a> ({$a->nbdest}) versendet.';
+$string['nomessagesend'] = 'Keine Benachrichtigung versendet.';
+$string['norecipient'] = 'Keine Empfänger.';
+$string['numbernotification'] = 'Anzahl der gesendeten Benachrichtigungen: {$a}';
+$string['returncourse'] = 'Zurück zum Kurs';
+$string['sender'] = 'Sender: ';
+$string['sendnotification'] = 'Benachrichtigung senden.';
+$string['subject'] = 'Betreff: ';
+$string['submit'] = 'Senden';
+
+// The following strings are part of the sent message
+$string['notification'] = 'Benachrichtigung: ';
+$string['msgsender'] = 'Die Ressource {$a->linkactivity} wurde von {$a->sender} im Kurs {$a->linkcourse} hervorgehoben.';
+
