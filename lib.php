94ca38128198e2a4f65a2c888ce342824e50b886
Guilloux <cecile.guilloux@silecs.info>
2018-03-28 15:04:29 +0200

    hijack the mod action menu with JS for Theme Boost

--- lib.php
+++ lib.php
@@ -8,16 +8,32 @@
 function local_resourcenotif_extend_navigation_course() {
     global $OUTPUT, $PAGE;
 
-    $html = '<li role="presentation">'
-        . '<a class="editing_notifications menu-action cm-edit-action" data-action="notifications" role="menuitem" href="'
+    $linkItem = '<a class="dropdown-item editing_notifications menu-action cm-edit-action" data-action="notifications" role="menuitem" href="'
         . htmlspecialchars(new moodle_url('/local/resourcenotif/resourcenotif.php', array('id' => '123XYZ321')))
         . '" title="' . htmlspecialchars(get_string("notifications")) . '">'
-        . $OUTPUT->pix_icon('t/email', get_string("notifications"), 'moodle', array('class' => 'iconsmall', 'title' => ''))
+        . $OUTPUT->pix_icon('t/email', get_string("notifications"))
         . '<span class="menu-action-text">' . htmlspecialchars(get_string("notifications")) . '</span>'
-        . '</a>'
-        . '</li>';
-    $enc = json_encode($html);
+        . '</a>';
 
+    //style Boost
+    $enc = json_encode($linkItem);
+    $PAGE->requires->js_init_code(<<<EOJS
+    var activities = document.querySelectorAll('.section-cm-edit-actions div[role="menu"]');
+    if (activities) {
+        for (var i = 0; i < activities.length; i++) {
+            var ul = activities[i];
+            var owner = ul.parentNode.parentNode.parentNode.getAttribute('data-owner');
+            if (owner) {
+                var id = owner.replace(/^#module-/, '');
+                ul.insertAdjacentHTML('beforeend', $enc.replace('123XYZ321', id));
+            }
+        }
+    }
+EOJS
+    , true);
+
+    //code style Clean
+    $enc = json_encode('<li role="presentation">' . $linkItem . '</li>');
     $PAGE->requires->js_init_code(<<<EOJS
     var activities = document.querySelectorAll('.section-cm-edit-actions ul[role="menu"]');
     if (activities) {
